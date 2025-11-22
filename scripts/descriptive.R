dbf79627fe4dbce4ebe54ef63b227c68e7443512
lucaseduardoveras <lucas_eduardo93@hotmail.com>
2025-11-22 18:14:41 -0500

    hist update

--- scripts/descriptive.R
+++ scripts/descriptive.R
@@ -35,6 +35,43 @@ stargazer(
     "Distancia a Transmilenio más cercano",
     "Baños"
   ),
-  digits = 3,
+  digits = 1,
   label = "tab:resumen_continuas"
 )
+
+train_categorical <- train %>% select(property_type,parqueadero_cubierto,zona_humeda,walking_closet,zona_verde,
+                                      chimenea,jacuzzi, piscina,balcon, gimnasio, parqueadero_cubierto,parqueadero_comunal,terraza) %>%
+  mutate(across(everything(), as.factor))
+
+stargazer(train_categorical,summary = TRUE)
+
+freq_list <- lapply(names(train_categorical), function(varname) {
+  data.frame(
+    Variable = varname,
+    Categoria = names(table(train_categorical[[varname]])),
+    Frecuencia = as.numeric(table(train_categorical[[varname]]))
+  )
+})
+
+freq_df <- do.call(rbind, freq_list) %>% mutate(Frecuencia = 100*Frecuencia/nrow(train))
+
+stargazer(
+  freq_df,
+  type = "latex",
+  summary = FALSE,
+  rownames = FALSE,
+  title = "Frecuencia de variables categóricas",
+  out = "stores/frecuencias_categoricas.tex",
+  label = "tab:frecuencias_categoricas",
+  covariate.labels = c("Variable", "Categoría", "Frecuencia"),
+  digits = 1
+)
+
+plot <- ggplot(train, aes(x = price*10^-9)) +
+  geom_histogram(fill = "lightblue", color = "black") +
+  labs(title = "Histograma de Precios",
+       x = "Precio",
+       y = "Frecuencia")
+
+ggsave("views/price_hist.png", plot,
+       width = 16, height = 9, units = "in",dpi = 100, bg = "white") 
